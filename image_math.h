2b03591c471fef6f985a6f5816ff5d1dc3cf9a00
AhmedMoamen62 <ahmedmoamen138@gmail.com>
2020-05-30 17:58:16 +0200

    initialize the repo

---
+++ image_math.h
@@ -0,0 +1,10 @@
+#ifndef IMAGE_MATH_H
+#define IMAGE_MATH_H
+
+// Maximum and Minimum
+int three_max(float a, float b, float c);
+int three_min(float a, float b, float c);
+int four_max(float a, float b, float c, float d);
+int four_min(float a, float b, float c, float d);
+
+#endif // IMAGE_MATH_H
---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "image.h"
+#include "args.h"
+
+int main()
+{
+
+    image img = load_image("data/dogsmall.jpg"); //data/dog.jpg
+    image image = bilinear_resize(img,img.w*4,img.h*4);
+    save_image(image,"editable_dog");
+    system("shotwell editable_dog.jpg");
+
+    //printf("D1 = %.6f \n", D1);
+
+    return 0;
+}
